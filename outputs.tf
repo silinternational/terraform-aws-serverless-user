6035f7a294b25d11fbf736b50bbe9c4d6d1be2fb
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-11 16:23:38 -0700

    minor corrections

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "aws_access_key_id" {
   value = aws_iam_access_key.serverless.id
 }
 output "aws_secret_access_key" {
-  value = aws_iam_access_key.serverless.secret
+  value     = aws_iam_access_key.serverless.secret
   sensitive = true
 }
 output "serverless_user_username" {
