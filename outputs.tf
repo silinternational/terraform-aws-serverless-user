863e9a1d1a71970693d108703f0c4a8c583a33e4
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-11 16:23:18 -0700

    add sensitive to access key

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "aws_access_key_id" {
 }
 output "aws_secret_access_key" {
   value = aws_iam_access_key.serverless.secret
+  sensitive = true
 }
 output "serverless_user_username" {
   value = aws_iam_user.serverless.name
