b6bf02bb42a78b34d551674b0aeba2b38863f4d9
briskt <3172830+briskt@users.noreply.github.com>
2024-08-08 10:54:29 +0800

    update aws provider to allow version 5

--- test/main.tf
+++ test/main.tf
@@ -22,14 +22,15 @@ provider "aws" {
 }
 
 locals {
-  aws_region = "us-east1"
+  aws_region = "us-east-1"
 }
 
 terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      version = "~ 4.0"
+      source  = "hashicorp/aws"
     }
   }
 }
